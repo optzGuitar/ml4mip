c2cc2df741e6d5f8b0af07fa76effbfd93bbf659
Leo Pinetzki <leo.leo11@hotmail.de>
2023-11-08 23:47:13 +0100

    contrast update

--- enums/contrast.py
+++ enums/contrast.py
@@ -1,11 +1,12 @@
 from enums.base_enum import BaseEnum
 
+
 class Contrasts(BaseEnum):
     flair = "FLAIR"
-    t1w = "T1w"
-    t1wce = "T1wCE"
-    t2w = "T2w"
-    
+    t1w = "T1"
+    t1ce = "T1CE"
+    t2w = "T2"
+
     @classmethod
     def values(cls):
         return sorted([c.value for c in cls])
