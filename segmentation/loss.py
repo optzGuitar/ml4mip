2ba66d430b8ae9f5fd66485d01b7b0b53d1fd218
optzGuitar <leo.leo11@hotmail.de>
2023-12-15 16:37:14 +0100

    a

--- segmentation/loss.py
+++ segmentation/loss.py
@@ -110,7 +110,7 @@ class CustomLoss(nn.Module):
         shape = y.shape
         ce_loss = self.ce(y_hat, y)
         tversky_loss = self.tversky_loss(
-            y_hat.view(*shape[:2], -1), y.view(*shape[:2], -1)).view(shape)
+            torch.argmax(y_hat, dim=1), torch.argmax(y, dim=1)).view(shape)
         gsl_loss = self.gsl_loss(y_hat, y)
         combined = (
             self.ce_weight * ce_loss +
