3735b1244e61740cf222f4f5d6cfa2ddba2de328
optzGuitar <leo.leo11@hotmail.de>
2023-12-15 22:09:19 +0100

    a

--- segmentation/loss.py
+++ segmentation/loss.py
@@ -102,10 +102,8 @@ class CustomLoss(nn.Module):
 
     def forward(self, y_hat: torch.Tensor, y: torch.Tensor, is_train: bool = True, log_fn=None) -> torch.Tensor:
         y_index = torch.argmax(y, dim=1)
-        print(y_index.shape, y_hat.view(
-            y_hat.shape[0], -1, y_hat.shape[1]).shape)
         ce_loss = self.ce(y_hat.view(
-            y_hat.shape[0], -1, y_hat.shape[1]), y_index)
+            y_hat.shape[0], -1, y_hat.shape[1]), y_index.view(y.shape[0], -1))
         tversky_loss = self.tversky_loss(
             y_hat, y_index)
         gsl_loss = self.gsl_loss(
