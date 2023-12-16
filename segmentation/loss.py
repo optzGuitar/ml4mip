7d64b8cb2f9c634f0dfec67fe1e18729f54c61f8
optzGuitar <leo.leo11@hotmail.de>
2023-12-16 18:32:43 +0100

    a

--- segmentation/loss.py
+++ segmentation/loss.py
@@ -96,7 +96,7 @@ class CustomLoss(nn.Module):
         self.ce = nn.BCELoss()
         self.tversky_loss = smp.losses.DiceLoss(
             mode="multilabel",  # I know this is a binary classification problem, but multilabel avoids having to copmute one-hot encoding twice
-            alpha=config.loss_config.tversky_alpha, beta=config.loss_config.tversky_beta)
+        )
         self.gsl_loss = GenSurfLoss()
         self.mse = nn.MSELoss()
         self.config = config
