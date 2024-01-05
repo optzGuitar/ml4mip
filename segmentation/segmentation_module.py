6fd2ff52dc24d2f73a753bd80b930de7d8afe43f
optzGuitar <leo.leo11@hotmail.de>
2024-01-05 11:41:27 +0100

    a

--- segmentation/segmentation_module.py
+++ segmentation/segmentation_module.py
@@ -94,8 +94,10 @@ class SegmentationModule(pl.LightningModule):
         tumor_mean = tumor_score.mean().item()
         whole_mean = whole_tumor.mean().item()
 
-        tumor_mean = 0 if tumor_mean.isnan() | tumor_mean.isinf() else tumor_mean
-        whole_mean = 0 if whole_mean.isnan() | whole_mean.isinf() else whole_mean
+        tumor_mean = 0 if tumor_mean is None | tumor_mean == float(
+            'inf') else tumor_mean
+        whole_mean = 0 if whole_mean is None | whole_mean == float(
+            'inf') else whole_mean
 
         self.log("val/tumor_score", tumor_mean)
         self.log("val/whole_tumor", whole_mean)
