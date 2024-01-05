b2aeedb0c44c314a03755a9799d7b0b113b45156
optzGuitar <leo.leo11@hotmail.de>
2024-01-05 11:50:55 +0100

    a

--- segmentation/loss.py
+++ segmentation/loss.py
@@ -97,7 +97,6 @@ class CustomLoss(nn.Module):
         self.config = config
 
     def forward(self, y_hat: torch.Tensor, y: torch.Tensor, is_train: bool = True, log_fn=None) -> torch.Tensor:
-        print(y_hat.shape, y.shape)
         ce_loss = self.ce(y_hat, y)
         gsl_loss = self.gsl_loss(
             y_hat, y, dtm=self.config.loss_config.dtm, alpha=self.config.loss_config.alpha)
